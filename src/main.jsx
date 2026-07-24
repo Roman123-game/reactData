0d0fbb8010b8d3d3791f3271d00c0f329729f21e
mrR <broitman.rom@gmail.com>
2026-07-24 19:53:41 +0300

    update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,159 +1,29 @@
 import { Application, Assets } from "pixi.js";
-
+import { Graphics } from "pixi.js";
 import { SlotMachine } from "./components/Reel/SlotMachine";
-
+import  {symbols } from "./objects/objects";
 // import cherry from "./assets/cherry.png";
 // import lemon from "./assets/lemon.png";
 // import bell from "./assets/bell.png";
 // import seven from "./assets/seven.png";
 // import diamond from "./assets/diamond.png";
-import bitcoin from "./assets/bitcoin.png";
-import etherium from "./assets/etherium.png";
-import litherium from "./assets/litherium.png";
-import tetherium from "./assets/tetherium.png";
-import shield from "./assets/shield.png";
-import safe from "./assets/case.png";
-import rocket from "./assets/rocket.png";
-import dog from "./assets/dog.png";
-import jackpot from "./assets/jackpot.png";
-import vault from "./assets/vault.png";
-import gain from "./assets/gain.png";
-import wild from "./assets/wild.png";
-import shiledtwo from "./assets/shiledtwo.png";
 
-import { Graphics } from "pixi.js";
 
 export class SlotFrame extends Graphics {
   constructor() {
     super();
-
     const width = 800;
     const height = 550;
-
     // Outer cabinet
-    this.roundRect(
-      0,
-      0,
-      width,
-      height,
-      30
-    )
-    .fill(0x111111)
-    .stroke({
-      width: 8,
-      color: 0xffcc00
-    });
-
+    this.roundRect( 0, 0,width,height,30).fill(0x111111).stroke({width: 8,color: 0xffcc00});
     // Inner slot window
-    this.roundRect(
-      25,
-      40,
-      750,
-      420,
-      20
-    )
-    .fill(0x000000)
-    .stroke({
-      width: 4,
-      color: 0xffffff
-    });
-
+    this.roundRect(25,40,750,420,20).fill(0x000000).stroke({width: 4,color: 0xffffff});
     // Bottom control area
-    this.roundRect(
-      25,
-      560,
-      550,
-      60,
-      15
-    )
-    .fill(0x222222)
-    .stroke({
-      width: 3,
-      color: 0xffcc00
-    });
+    this.roundRect(25,560,550,60,15).fill(0x222222).stroke({width: 3,color: 0xffcc00 });
   }
 }
 
-const symbols = [
-  {
-    name: "Bitcoin",
-    texture: bitcoin,
-    payout: 20,
-    type: "normal",
-  },
-  {
-    name: "Ethereum",
-    texture: etherium,
-    payout: 25,
-    type: "normal",
-  },
-  {
-    name: "Litecoin",
-    texture: litherium,
-    payout: 15,
-    type: "normal",
-  },
-  {
-    name: "Tether",
-    texture: tetherium,
-    payout: 10,
-    type: "normal",
-  },
-  {
-    name: "Shield",
-    texture: shield,
-    payout: 30,
-    type: "normal",
-  },
-  {
-    name: "Safe",
-    texture: safe,
-    payout: 35,
-    type: "normal",
-  },
-  {
-    name: "Rocket",
-    texture: rocket,
-    payout: 40,
-    type: "normal",
-  },
-  {
-    name: "Dog",
-    texture: dog,
-    payout: 50,
-    type: "normal",
-  },
-  {
-    name: "Jackpot",
-    texture: jackpot,
-    payout: 100,
-    type: "normal",
-  },
-  {
-    name: "Vault",
-    texture: vault,
-    payout: 80,
-    type: "normal",
-  },
-  {
-    name: "Gain",
-    texture: gain,
-    payout: 60,
-    type: "normal",
-  },
-  {
-    name: "Wild",
-    texture: wild,
-    payout: 0,
-    type: "wild",
-  },
-  {
-    name: "Shield Two",
-    texture: shiledtwo,
-    payout: 45,
-    type: "normal",
-  },
-];
+
 
 const app = new Application();
 
---
+++ src/objects/objects.js
@@ -0,0 +1,94 @@
+import bitcoin from "../assets/bitcoin.png";
+import etherium from "../assets/etherium.png";
+import litherium from "../assets/litherium.png";
+import tetherium from "../assets/tetherium.png";
+import shield from "../assets/shield.png";
+import safe from "../assets/case.png";
+import rocket from "../assets/rocket.png";
+import dog from "../assets/dog.png";
+import jackpot from "../assets/jackpot.png";
+import vault from "../assets/vault.png";
+import gain from "../assets/gain.png";
+import wild from "../assets/wild.png";
+import shiledtwo from "../assets/shiledtwo.png"; 
+ 
+ export const symbols = [
+  {
+    name: "Bitcoin",
+    texture: bitcoin,
+    payout: 20,
+    type: "normal",
+  },
+  {
+    name: "Ethereum",
+    texture: etherium,
+    payout: 25,
+    type: "normal",
+  },
+  {
+    name: "Litecoin",
+    texture: litherium,
+    payout: 15,
+    type: "normal",
+  },
+  {
+    name: "Tether",
+    texture: tetherium,
+    payout: 10,
+    type: "normal",
+  },
+  {
+    name: "Shield",
+    texture: shield,
+    payout: 30,
+    type: "normal",
+  },
+  {
+    name: "Safe",
+    texture: safe,
+    payout: 35,
+    type: "normal",
+  },
+  {
+    name: "Rocket",
+    texture: rocket,
+    payout: 40,
+    type: "normal",
+  },
+  {
+    name: "Dog",
+    texture: dog,
+    payout: 50,
+    type: "normal",
+  },
+  {
+    name: "Jackpot",
+    texture: jackpot,
+    payout: 100,
+    type: "normal",
+  },
+  {
+    name: "Vault",
+    texture: vault,
+    payout: 80,
+    type: "normal",
+  },
+  {
+    name: "Gain",
+    texture: gain,
+    payout: 60,
+    type: "normal",
+  },
+  {
+    name: "Wild",
+    texture: wild,
+    payout: 0,
+    type: "wild",
+  },
+  {
+    name: "Shield Two",
+    texture: shiledtwo,
+    payout: 45,
+    type: "normal",
+  },
+];
